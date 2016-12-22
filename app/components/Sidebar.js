6df2aee3713a99c1a13f7bf7f3e4cabc065fe730
Mark <mlang41110@yahoo.com>
2016-12-22 01:01:32 -0500

    turned Sidebar into a stateless functional component

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -2,17 +2,13 @@ import React from 'react';
 import NewItemFormContainer from './NewItemFormContainer';
 import Radium from 'radium';
 
-class Sidebar extends React.Component {
-    render() {
-        return (
-            <div id = "sidebar"
-                className = {this.props.outerElementClassName}>
-                <h2 className = "text-center">Add New Item</h2>
-                <NewItemFormContainer
-                    addItem = {this.props.addItem} />
-            </div>
-        );
-    }
-}
+const Sidebar = props => (
+    <div id = "sidebar"
+        className = {props.outerElementClassName}>
+        <h2 className = "text-center">Add New Item</h2>
+        <NewItemFormContainer
+            addItem = {props.addItem} />
+    </div>
+);
 
 export default Radium(Sidebar);
